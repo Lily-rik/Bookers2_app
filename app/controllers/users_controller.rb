8f5069acb300df87f79b920e2ccaa5bc966ae035
EC2 Default User <violet794.py16@gmail.com>
2021-04-18 11:49:23 +0000

    [add]5-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
 
+
+  def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+  end
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
     @books = @user.books.all
   end
 
