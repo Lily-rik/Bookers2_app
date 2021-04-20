9a8768fe316fb05c8c4ef90f2fb32212ee10bba9
EC2 Default User <violet794.py16@gmail.com>
2021-04-20 14:06:32 +0000

    [add]5-10

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'homes#top'
-  get 'about' => 'homes#about'
+  get 'home/about' => 'homes#about'
 
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 
